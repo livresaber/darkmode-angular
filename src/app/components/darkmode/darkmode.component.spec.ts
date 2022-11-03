5b16ad365e88e438d30119a136aa828cef341e81
lucasferreiralimax <lucasferreiralimax@gmail.com>
2022-11-03 11:28:06 -0300

    test(unit): adjustment validation button and icon

--- src/app/components/darkmode/darkmode.component.spec.ts
+++ src/app/components/darkmode/darkmode.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 import { DarkmodeComponent } from './darkmode.component';
 
@@ -20,30 +21,52 @@ describe('DarkmodeComponent', () => {
   it('should create', async () => {
     expect(component).toBeTruthy();
   });
-  it('should light mode', async () => {
-    fixture = TestBed.createComponent(DarkmodeComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+  it('should light mode', fakeAsync(async () => {
+    // const btnMode = fixture.nativeElement.querySelector('.btn-mode')
+    // const icon = btnMode.querySelector('[aria-label="Icon Light Mode"]')
 
-    const btnMode = fixture.nativeElement.querySelector('.btn-mode')
-    const icon = btnMode.querySelector('[aria-label="Icon Light Mode"]')
-    expect(btnMode).toBeTruthy();
-    expect(icon).toBeTruthy();
-    expect(btnMode.textContent.trim()).toBe('LightMode');
-  });
-  it('should dark mode', fakeAsync(async () => {
-    fixture = TestBed.createComponent(DarkmodeComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-
-    const btnMode = fixture.nativeElement.querySelector('.btn-mode')
-    btnMode.click();
-    tick(2000);
-    fixture.detectChanges();
-
-    const icon = btnMode.querySelector('[aria-label="Icon Dark Mode"]')
-    expect(btnMode).toBeTruthy();
-    expect(icon).toBeTruthy();
-    expect(btnMode.textContent.trim()).toBe('DarkMode');
+    // expect(btnMode).toBeTruthy();
+    expect(modeButton()).toBe('LightMode');
+    expect(buttonIcon('[aria-label="Icon Light Mode"]')).toBeTruthy();
   }));
+  it('should dark mode', fakeAsync(async () => {
+    fixture.detectChanges();
+    tick();
+
+    const btnMode = fixture.debugElement.query(By.css('.btn-mode')).nativeElement
+    btnMode.dispatchEvent(new Event('click'));
+
+    tick();
+
+    expect(modeButton()).toBe('DarkMode');
+    expect(buttonIcon('[aria-label="Icon Dark Mode"]')).toBeTruthy();
+  //   tick(2000);
+  //   fixture.detectChanges();
+  //   expect(btnMode).toBeTruthy();
+  //   expect(modeButton()).toEqual('DarkMode');
+  }));
+
+  function modeButton() {
+    fixture.detectChanges();
+    tick();
+
+    let result = fixture.debugElement.query(
+      By.css('.btn-mode')
+    ).nativeElement.textContent.trim();
+
+    tick();
+    return result;
+  }
+
+  function buttonIcon(value: string) {
+    fixture.detectChanges();
+    tick();
+
+    let result = fixture.debugElement.query(
+      By.css(value)
+    ).nativeElement;
+
+    tick();
+    return result;
+  }
 });
