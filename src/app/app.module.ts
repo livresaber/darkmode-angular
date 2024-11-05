bfba83f2bd1dd32b30d528cf38ce4e9ff4194677
lucasferreiralimax <lucasferreiralimax@gmail.com>
2024-11-04 23:18:23 -0300

    add nucleus-box

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { DarkmodeComponent } from 'projects/darkmode-angular/src/public-api';
-import { NucleusAngularApp } from 'nucleus-angular';
+import { NucleusAngularApp, NucleusBox } from 'nucleus-angular';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, DarkmodeComponent, NucleusAngularApp],
+  imports: [BrowserModule, DarkmodeComponent, NucleusAngularApp, NucleusBox],
   providers: [],
   bootstrap: [AppComponent],
 })
